c39fa4e68793faee12a0ddea232a79dce9664543
vegasword <a.perche@student.isartdigital.com>
2023-01-31 19:11:17 +0100

    Began to follow Vulkan tutorial and stopped at Vulkan initialization

---
+++ src/main.cpp
@@ -0,0 +1,105 @@
+#include <vulkan/vulkan.h>
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+class Application
+{
+public:
+    void run()
+    {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+
+private:
+    GLFWwindow* window;
+
+    VkInstance vkInstance;
+    
+    void initWindow()
+    {
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        window = glfwCreateWindow(800, 600, "Vulkan", nullptr, nullptr);
+    }
+
+    VkResult createInstance()
+    {
+        // Metadata of the application (mostly use by the driver for config).
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        /*
+         * Tells Vulkan driver which global extensions and validations layers
+         * we want to use.
+         * Global extensions are a set of additional Vulkan features.
+         * Validations layers are optional components that hook into Vulkan
+         * function calls to apply additional operations like checking the
+         * values of parameters against the specification to detect misuse or
+         * the thread safety...
+        */
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+        
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledLayerCount = 0;
+
+        // We finally create the Vulkan instance.
+        VkResult result = vkCreateInstance(&createInfo, nullptr, &vkInstance);
+        if (result != VK_SUCCESS)
+            throw std::runtime_error("Failed to create Vulkan instance");
+
+        return result;
+    }
+    
+    void initVulkan()
+    {
+        createInstance();
+    }
+
+    void mainLoop()
+    {
+        while (!glfwWindowShouldClose(window))
+        {
+            glfwPollEvents();
+        }
+    }
+
+    void cleanup()
+    {
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
+};
+
+int main() {
+    Application app;
+
+    try {
+        app.run();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
