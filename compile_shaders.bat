20150598609d901221a8d8b2f6a990d51a1b8976
vegasword <a.perche@student.isartdigital.com>
2023-02-10 12:37:11 +0100

    Updated shader compilation script

--- compile_shaders.bat
+++ compile_shaders.bat
@@ -1,2 +1,5 @@
-glslc data/shader.vert -o data/vert.spv
-glslc data/shader.frag -o data/frag.spv
+@echo off
+pushd .\\data\\shaders
+for %%f in (*.vert) do glslc -o %%~nf.spv %%f
+for %%f in (*.frag) do glslc -o %%~nf.spv %%f
+popd
